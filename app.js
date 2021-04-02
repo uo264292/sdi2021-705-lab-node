cd6c26deec11bd8414a07c68466c56f834c2f059
sergio cimadevilla torga <uo264292@uniovi.es>
2021-04-03 00:23:09 +0200

    SDI-2021-705-8.1-Acceso a datos de Mongo desde Node.js.

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // Modulos
 let express = require("express");
 let app = express();
+
+let mongo = require("mongodb");
+
 app.use(express.static('public'));
 let swig = require('swig');
 let bodyParser = require('body-parser');
@@ -10,10 +13,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Variables
 app.set('port', 8081);
+app.set('db', 'mongodb://admin:sdi@tiendamusica-shard-00-00.3wbjm.mongodb.net:27017,tiendamusica-shard-00-01.3wbjm.mongodb.net:27017,tiendamusica-shard-00-02.3wbjm.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-7q72ww-shard-0&authSource=admin&retryWrites=true&w=majority');
 
 //Rutas/controladores por lógica
 require("./routes/rusuarios.js")(app,swig); // (app, param1, param2, etc.)
-require("./routes/rcanciones.js")(app,swig); // (app, param1, param2, etc.)
+require("./routes/rcanciones.js")(app,swig,mongo); // (app, param1, param2, etc.)
 require("./routes/rautores.js")(app,swig);
 
 app.listen(app.get('port'),function (){
